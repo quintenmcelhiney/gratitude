ab9ede35483a0b6d4fd97730d815902f6047dbb3
Quinten McElhiney <48164572+quintenmcelhiney@users.noreply.github.com>
2022-10-19 01:27:08 +0000

    Update grat.js

--- grat.js
+++ grat.js
@@ -1,43 +1,6 @@
-var meds = [
-        "my breath",
-        "my pulse",
-        "my voice",
-        "my brain",
-        "my eyes",
-        "my legs",
-        "my hands",
-        "touch",
-        "my friends",
-        "my sister",
-        "my mother",
-        "my brother",
-        "my father",
-        "my ancestors",
-        "my history",
-        "my teachers",
-        "my family",
-        "my home",
-        "the air",
-        "the light",
-        "the universe",
-        "existence",
-        "mind",
-        "movement",
-        "my animal friends",
-        "art",
-        "language",
-        "the vast",
-        "the infinite",
-        "my cosmic insignificance".
-        "culture",
-        "the beautiful",
-        "science"
-        
-    ];
-
 
 function run(){
-    console.log(meds);
+    //console.log(meds);
     changeColor(randomColor());
     setTimeout(run, Math.random()*6000);
 }
