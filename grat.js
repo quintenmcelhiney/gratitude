a1fd7aee74bf1f704a3c00b5ed7e4b3c90537cf5
Quinten McElhiney <48164572+quintenmcelhiney@users.noreply.github.com>
2022-10-19 00:34:21 +0000

    Update grat.js

--- grat.js
+++ grat.js
@@ -49,48 +49,7 @@ function randomColor(){
     return colorArray[Math.floor(Math.random()*colorArray.length)];
 }
 
-function meditation(){
-     
-    var meditations = [
-        "my breath",
-        "my pulse",
-        "my voice",
-        "my brain",
-        "my eyes",
-        "my legs",
-        "touch",
-        "my friends",
-        "my sister",
-        "my mother",
-        "my brother",
-        "my father",
-        "my ancestors",
-        "my history",
-        "my teachers",
-        "my family",
-        "my home",
-        "the air",
-        "the light",
-        "the universe",
-        "existence",
-        "mind",
-        "movement",
-        "my animal friends",
-        "art",
-        "language",
-        "the vast",
-        "the infinite",
-        "my cosmic insignificance".
-        "culture",
-        "the beautiful",
-        "science"
-        
-    ];
-      
-    var rn = Math.floor(Math.random()*meditations.length);
-    document.getElementById('meditation').innerHTML = meditations[rn];
 
-} 
 
 function changeColor(color){
   document.body.style.background = color;
